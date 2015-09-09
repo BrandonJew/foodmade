5da1aeec0b24bc64fea8acd20e6701e01e01e5ef
BrandonJew <bjew95@live.com>
2015-09-08 19:45:16 -0700

    user authentication and password reset added

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 gem 'bcrypt', '~> 3.1.10'
 gem 'bootstrap-sass', '3.3.5'
 gem 'mysql2'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824221956) do
+ActiveRecord::Schema.define(version: 20150908024631) do
 
   create_table "recipients", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -24,12 +24,18 @@ ActiveRecord::Schema.define(version: 20150824221956) do
   add_index "recipients", ["email"], name: "index_recipients_on_email", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "email",           limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "password_digest", limit: 255
-    t.string   "remember_digest", limit: 255
+    t.string   "name",              limit: 255
+    t.string   "email",             limit: 255
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "password_digest",   limit: 255
+    t.string   "remember_digest",   limit: 255
+    t.boolean  "admin",                         default: false
+    t.string   "activation_digest", limit: 255
+    t.boolean  "activated",                     default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
