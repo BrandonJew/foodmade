34be98d65529d04a57a0e3fa9c17b8d02ae84b6b
BrandonJew <bjew95@live.com>
2015-09-09 23:26:58 -0700

    this and that

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(admin: false).paginate(page: params[:page], :per_page => 15)
   end
 
   # GET /users/1
